2c34aa2253bef3e98a14adb71d5e7d3a3cb49710
legato111 <bbahmad@gmail.com>
2016-10-04 07:07:58 -0400

    Init

---
+++ views/top.js
@@ -0,0 +1,42 @@
+define([
+	"app"
+],function(app){
+
+	var header = {
+		type:"header", template:app.config.name
+	};
+
+	var menu = {
+		view:"menu", id:"top:menu", 
+		width:180, layout:"y", select:true,
+		template:"<span class='webix_icon fa-#icon#'></span> #value# ",
+		data:[
+			{ value:"DashBoard", 					id:"start",						href:"#!/top/start", 		icon:"envelope-o" },
+			{ value:"Total Consults", 				id:"totalConsults",				href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Patient Satisfaction", 		id:"patientSatisfaction",		href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Slot Utilization", 			id:"slotUtilization",			href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Total Visits", 				id:"totalVisits",				href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Cognitive Diagnoses", 			id:"cogDiagnoses",				href:"#!/top/data", 		icon:"briefcase" },
+			//will include cognitive diagnoses by percentage, MOCA score, SLUMS, etc - "cognitive severity index/composition" of sorts
+			{ value:"Data", 						id:"data",						href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Data2", 						id:"data2",						href:"#!/top/data2", 		icon:"briefcase" }
+		]
+	};
+
+	var ui = {
+		type:"line", cols:[
+			{ type:"clean", css:"app-left-panel",
+				padding:10, margin:20, borderless:true, rows: [ header, menu ]},
+			{ rows:[ { height:10}, 
+				{ type:"clean", css:"app-right-panel", padding:4, rows:[
+					{ $subview:true } 
+				]}
+			]}
+		]
+	};
+
+	return {
+		$ui: ui,
+		$menu: "top:menu"
+	};
+});
