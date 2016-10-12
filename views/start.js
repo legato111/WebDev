b2e2ad09e51626757a21778e3ed1e71d078d17ff
legato111 <bbahmad@gmail.com>
2016-10-11 23:13:41 -0400

    Dashboard more complete

--- views/start.js
+++ views/start.js
@@ -2,10 +2,39 @@ define([
 	"models/chartdata"
 ],function(chartdata){
 
-	var chart1 = {
+	// var topDiagnoses = {
+	// 	view:"chart",//, autoConfig:true
+	// 	type:"bar",
+	// 	border:true,
+	// 	data:chartdata.data,
+	// 	series:[
+ //        {   // 1st series
+ //            value:"#year#",
+ //            color: "#58dccd", 
+ //            tooltip:{
+ //                template:"#year#"
+ //            }
+ //        },
+ //        {   //2nd series
+ //            value:"#votes#",
+ //            color:"#a7ee70",
+ //            tooltip:{
+ //                template:"#votes#"
+ //            }
+ //        },
+ //        {   // 3rd series
+ //            value:"#rating3#",  
+ //            color:"#36abee",
+ //            tooltip:{
+ //                template:"#rating#"
+ //            }
+ //    	}],
+ //    };
+
+	var topDiagnoses = {
 		view:"chart",//, autoConfig:true
 		type:"bar",
-		border:true,
+		border:false,
 		data:chartdata.data,
 		series:[
         {   // 1st series
@@ -14,38 +43,149 @@ define([
             tooltip:{
                 template:"#year#"
             }
-        },
-        {   //2nd series
-            value:"#votes#",
-            color:"#a7ee70",
-            tooltip:{
-                template:"#votes#"
-            }
-        },
-        {   // 3rd series
-            value:"#rating3#",  
-            color:"#36abee",
-            tooltip:{
-                template:"#rating#"
-            }
-    	}],
+        }],
     };
 
-    var hihi = {
-		type:"header", template:"hihi"
+    var topDiagnosesTitle = {
+		template:"Top ICD10 Diagnoses", height:30
 	};
 
-    var examplepie = {
-    	view: "chart", type:"pie", data:[{sales:"0.5", month:"Jan", color: "#ee3639"}, {sales:"0.5", month:"Feb", color: "#ee3639"}], value: "#sales#"
+    var uniquePatients = {
+		view:"chart",//, autoConfig:true
+		type:"line",
+		border:false,
+		data:chartdata.data,
+		xAxis:{title:"Month"},
+		yAxis:{title:"#Patients"},
+		series:[
+        {   // 1st series
+            value:"#year#",
+            color: "#58dccd", 
+            tooltip:{
+                template:"#year#"
+            },
+            eventRadius: 10
+        },
+        ],
     };
+
+	var uniquePatientsTitle = {
+		template:"Unique Patients", height:30
+	};
+
+    var totalVisits = {
+		view:"chart",//, autoConfig:true
+		type:"line",
+		border:false,
+		data:chartdata.data,
+		xAxis:{title:"Month"},
+		yAxis:{title:"#Visits"},
+		series:[
+        {   // 1st series
+            value:"#year#",
+            color: "#58dccd", 
+            tooltip:{
+                template:"#year#"
+            },
+            eventRadius: 10
+        },
+        ],
+    };
+
+	var totalVisitsTitle = {
+		template:"Total Clinic Visits", height:30
+	};
+
+    var slotUtilization = {
+		view:"chart",//, autoConfig:true
+		type:"line",
+		border:false,
+		data:chartdata.data,
+		xAxis:{title:"Month"},
+		yAxis:{title:"%Slot Utilization"},
+		series:[
+        {   // 1st series
+            value:"#year#",
+            color: "#58dccd", 
+            tooltip:{
+                template:"#year#"
+            },
+            eventRadius: 10
+        },
+        ],
+    };
+
+	var slotUtilizationTitle = {
+		template:"% Slot Utilization", height:30
+	};
+
+    var highRiskMeds = {
+		view:"chart",//, autoConfig:true
+		type:"line",
+		border:false,
+		data:chartdata.data,
+		xAxis:{title:"Month"},
+		yAxis:{title:"%On High Risk Medication"},
+		series:[
+        {   // 1st series
+            value:"#year#",
+            color: "#58dccd", 
+            tooltip:{
+                template:"#year#"
+            },
+            eventRadius: 10
+        },
+        ],
+    };
+
+	var highRiskMedsTitle = {
+		template:"> 1 Visit on High Risk Medication", height:30
+	};
+
+	var totalConsults = {
+		view:"chart",//, autoConfig:true
+		type:"line",
+		border:false,
+		data:chartdata.data,
+		xAxis:{title:"Month"},
+		yAxis:{title:"Total Consults"},
+		series:[
+        {   // 1st series
+            value:"#year#",
+            color: "#58dccd", 
+            tooltip:{
+                template:"#year#"
+            },
+            eventRadius: 10
+        },
+        ],
+    };
+
+	var totalConsultsTitle = {
+		template:"Total Consults", height:30
+	};
+
+    var topConsultingDepts = {
+    	view: "chart", type:"pie", data:[{sales:"0.3", month:"Primary Care", color: "#ee1602", id:"hi1"}, {sales:"0.3", month:"Neurology", color: "#ee3639", id:"hi2"}, 
+    	{sales:"0.4", month:"Inpatient", color: "#ee2454", id:"hi3"}], value: "#sales#", label: "#month#"
+    };
+
+	var topConsultingDeptsTitle = {
+		template:"Top Consulting Departments", height:30
+	};
+
+ //    var hihi = {
+	// 	type:"header", template:"hihi"
+	// };
 
 	var ui = {
 		rows:[
 	        {
 	        	cols:[
-	                {cols:[chart1]},
+	                {rows:[uniquePatientsTitle, uniquePatients]},
 	                // {type:"header", template:"hihi"}
-	                {cols:[chart1]}
+	                {rows:[totalVisitsTitle, totalVisits]},
+	                {rows:[slotUtilizationTitle, slotUtilization]}
 	            ]
 	        },
 	        // {
@@ -53,14 +193,17 @@ define([
 	        // },
 	        {
 	        	cols:[
-	                {view: "chart", type:"pie", data:[{sales:"0.5", month:"Jan", color: "#ee3639"}, {sales:"0.5", month:"Feb", color: "#ee3639"}], value: "#sales#"},
-	                {rows:[hihi, examplepie]}
+	                //{view: "chart", type:"pie", data:[{sales:"0.5", month:"Jan", color: "#ee3639"}, {sales:"0.5", month:"Feb", color: "#ee3639"}], value: "#sales#"},
+	                {rows:[totalConsultsTitle, totalConsults]},
+	                {rows:[topDiagnosesTitle, topDiagnoses]},
+	                {rows:[highRiskMedsTitle, highRiskMeds]}
 	            ]
 	        },
 	        {
 	            cols:[
-	                {id:"hihi1", rows:[chart1]},
-	                {rows:[chart1]}
+	                {rows:[topConsultingDeptsTitle, topConsultingDepts]},
+	                {rows:[highRiskMedsTitle, highRiskMeds]},
+	                {rows:[highRiskMedsTitle, topDiagnoses]}
 	            ]
 	        }
 	    ]
--- views/top.js
+++ views/top.js
@@ -2,6 +2,16 @@ define([
 	"app"
 ],function(app){
 
+	var toolbar = {
+	view:"toolbar",
+    id:"myToolbar",
+    cols:[
+        { view:"button", id:"LoadBut", value:"Load", width:100, align:"left" },
+        { view:"button", value:"Save", width:100, align:"center" },
+        { view:"button", value:"Info", width:100, align:"right" }]
+
+    };
+
 	var header = {
 		type:"header", template:app.config.name
 	};
@@ -12,11 +22,11 @@ define([
 		template:"<span class='webix_icon fa-#icon#'></span> #value# ",
 		data:[
 			{ value:"DashBoard", 					id:"start",						href:"#!/top/start", 		icon:"envelope-o" },
-			//{ value:"Total Consults", 				id:"totalConsults",				href:"#!/top/data", 		icon:"briefcase" },
-			//{ value:"Patient Satisfaction", 		id:"patientSatisfaction",		href:"#!/top/data", 		icon:"briefcase" },
-			//{ value:"Slot Utilization", 			id:"slotUtilization",			href:"#!/top/data", 		icon:"briefcase" },
-			//{ value:"Total Visits", 				id:"totalVisits",				href:"#!/top/data", 		icon:"briefcase" },
-			//{ value:"Cognitive Diagnoses", 			id:"cogDiagnoses",				href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Total Consults", 				id:"totalConsults",				href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Patient Satisfaction", 		id:"patientSatisfaction",		href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Slot Utilization", 			id:"slotUtilization",			href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Total Visits", 				id:"totalVisits",				href:"#!/top/data", 		icon:"briefcase" },
+			{ value:"Cognitive Diagnoses", 			id:"cogDiagnoses",				href:"#!/top/data", 		icon:"briefcase" },
 			//will include cognitive diagnoses by percentage, MOCA score, SLUMS, etc - "cognitive severity index/composition" of sorts
 			{ value:"Data", 						id:"data",						href:"#!/top/data", 		icon:"briefcase" },
 			//{ value:"ChartData",					id:"chartData",					href:"#!/top/dashChart1",	icon:"briefcase" },
@@ -29,7 +39,7 @@ define([
 			{ type:"clean", css:"app-left-panel",
 				padding:10, margin:20, borderless:true, rows: [ header, menu ]},
 			{ rows:[ { height:10}, 
-				{ type:"clean", css:"app-right-panel", padding:4, rows:[
+				{ type:"clean", css:"app-right-panel", padding:4, borderless:true, rows:[toolbar,
 					{ $subview:true } 
 				]}
 			]}
